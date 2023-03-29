875d93a4fb8571481a719a5a9e604fdb264dcc11
felipefiih97 <felipebarros19977@gmail.com>
2023-03-29 15:43:11 -0300

    Update-validation1.3

--- Segunda-fase-alura-T4/6-Validação-de-form-js-html/js/validacao.js
+++ Segunda-fase-alura-T4/6-Validação-de-form-js-html/js/validacao.js
@@ -15,7 +15,7 @@ const validadores = {
 em formato calendário, eu pego apenas o valor do input.  */
 function validaDataNascimento(input){
     const dataRecebida = new Date(input.value);
-    const mensagem = '';
+    var mensagem = '';
 
    if(!maiorQue18(dataRecebida)){ 
        mensagem = "Você deve ser maior que 18 anos para se cadastrar."    
